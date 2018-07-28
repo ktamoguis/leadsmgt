e3da15546d57670361285e634f8d232d6f96f110
ktamoguis <ktamoguis@gmail.com>
2018-07-28 15:08:39 +0800

    new class methods for leads

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,4 +1,16 @@
 class Lead < ApplicationRecord
   belongs_to :agent
   belongs_to :industry
+
+  def self.converted
+    self.where("status = converted")
+  end
+
+  def self.active
+    self.where("status = Go")
+  end
+
+  
+
+
 end
