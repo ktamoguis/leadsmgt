60914087afef695b206179df365f11bd0389fa71
ktamoguis <ktamoguis@gmail.com>
2018-07-28 19:04:53 +0800

    added edit controller for leads, edited show view for agent

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -6,24 +6,35 @@ class LeadsController < ApplicationController
   end
 
   def create
-    binding.pry
+    #binding.pry
     @lead = Lead.new(leads_params)
     @lead.agent = current_user
-    binding.pry
+    #binding.pry
     @lead.save
-    binding.pry
+    #binding.pry
 
     redirect_to agent_path(current_user)
   end
 
   def edit
+    binding.pry
     @lead = Lead.find_by(id: params[:id])
+  end
+
+  def update
+    binding.pry
+    @lead = Lead.find(params[:id])
+    binding.pry
+    @lead.update(leads_params)
+    binding.pry
+
+    redirect_to agent_path(current_user)
   end
 
 
   private
   def leads_params
-    params.require(:lead).permit(:name, :status, :industry_id, industry_attributes:[:name])
+    params.require(:lead).permit(:name, :status, :booked_loans, :industry_id, industry_attributes:[:name])
   end
 
   def current_user
