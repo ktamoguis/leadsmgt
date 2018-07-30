9af9d97de57560ff07b850d6f8b83fd517cb3aa7
ktamoguis <ktamoguis@gmail.com>
2018-07-30 17:42:57 +0800

    added control checks for industries and regions controllers

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -2,6 +2,7 @@ class RegionsController < ApplicationController
 
   def show
     binding.pry
+    control_check
     @agent = Agent.find_by(id: session[:agent_id])
     @region = Region.find_by(id: params[:id])
     if params[:status].nil? || params[:status] == ""
@@ -13,12 +14,46 @@ class RegionsController < ApplicationController
 
   def index
     binding.pry
+    control_check
     @agent = Agent.find_by(id: session[:agent_id])
     if @agent.manager
       @regions = Region.all
     else
       redirect_to agent_path(@agent)
     end
+  end
+
+  private
+
+  def control_check
+    if !logged_in?
+      not_logged_in_action
+    elsif agent_is_not_current_user?
+      agent_not_current_user_action
+    end
+  end
+
+
+  def logged_in?
+    !!session[:agent_id]
+  end
+
+  def not_logged_in_action
+    flash[:notice] = "Agent not logged in"
+    redirect_to '/'
+  end
+
+
+  def agent_is_not_current_user?
+    if params[:agent_id].nil?
+    else
+      Agent.find_by(id: params[:agent_id]) != current_user
+    end
+  end
+
+  def agent_not_current_user_action
+    flash[:notice] = "Lead does not belong to current agent"
+    redirect_to agent_path(session[:agent_id])
   end
 
 end
