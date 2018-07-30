8b9de4c856f8b0634b37ad73ba708bb960ce387c
ktamoguis <ktamoguis@gmail.com>
2018-07-30 13:54:23 +0800

    added index views for industries, view region and industry summaries for manager

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -3,13 +3,19 @@ class IndustriesController < ApplicationController
   def show
     binding.pry
     @agent = Agent.find_by(id: session[:agent_id])
+    @industry = Industry.find_by(id: params[:id])
+    if params[:status].nil? || params[:status] == ""
+      @leads = Lead.by_manager_industry(@industry.id)
+    else
+      @leads = Lead.by_industry(@industry.id, params[:status])
+    end
+  end
+
+  def index
+    binding.pry
+    @agent = Agent.find_by(id: session[:agent_id])
     if @agent.manager
-      @industry = Industry.find_by(id: params[:id])
-      if params[:status].nil? || params[:status] == ""
-        @leads = Lead.by_manager_industry(@industry.id)
-      else
-        @leads = Lead.by_industry(@industry.id, params[:status])
-      end
+      @industries = Industry.all
     else
       redirect_to agent_path(@agent)
     end
--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -3,13 +3,19 @@ class RegionsController < ApplicationController
   def show
     binding.pry
     @agent = Agent.find_by(id: session[:agent_id])
+    @region = Region.find_by(id: params[:id])
+    if params[:status].nil? || params[:status] == ""
+      @leads = Lead.by_manager_region(@region.id)
+    else
+      @leads = Lead.by_region(@region.id, params[:status])
+    end
+  end
+
+  def index
+    binding.pry
+    @agent = Agent.find_by(id: session[:agent_id])
     if @agent.manager
-      @region = Region.find_by(id: params[:id])
-      if params[:status].nil? || params[:status] == ""
-        @leads = Lead.by_manager_region(@region.id)
-      else
-        @leads = Lead.by_region(@region.id, params[:status])
-      end
+      @regions = Region.all
     else
       redirect_to agent_path(@agent)
     end
